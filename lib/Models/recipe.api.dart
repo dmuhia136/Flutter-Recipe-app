aaaef4ee909aacb7d7760d2caa4a7945c092864b
dmuhia136 <dmuhia136@gmail.com>
2022-02-15 11:16:09 +0300

    Flutter recipe app multi platform

---
+++ lib/Models/recipe.api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:recipes/Models/recipe.dart';
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "090152b6demshcb0e91982bd05aap1316f8jsn60443e8f3678",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
